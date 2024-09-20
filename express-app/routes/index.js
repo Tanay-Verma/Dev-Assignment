2cdba59a095912bba89ff243c5411654551502ee
Tanay <tanayverma98@gmail.com>
2024-09-20 10:04:58 +0530

    Add cors and changes the response for the /users route in express-app

--- express-app/routes/index.js
+++ express-app/routes/index.js
@@ -2,5 +2,5 @@ import {Router} from "express"
 export const router = Router();
 
 router.get("/users", (req, res) => {
-    res.json([{name: "John"}, {name: "Jane"}])
+    res.json([{name: "John", age: 30, email: "a@a.com"}, {name: "Jane", age: 25, email: "b@b.com"}])
 })
